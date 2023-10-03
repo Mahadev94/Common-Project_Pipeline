70e5a29fc3a37bcba0edd7fc700627331328396b
Mahadev Kharmate <mahadev.mk294@gmail.com>
2023-10-03 15:36:16 +0530

    load object func added

--- source/utils/utils.py
+++ source/utils/utils.py
@@ -54,4 +54,11 @@ def evaluate_model(X_train,Y_train,X_test,Y_test,models):
         logging.info("Erroe in evaluate models")
         raise CustomException(e,sys)
     
-    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+        
+    except Exception as e:
+        logging.info("Exception occered in load objet utils function")
+        raise CustomException(e,sys)
